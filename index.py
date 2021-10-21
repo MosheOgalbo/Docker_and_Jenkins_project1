a2c1dcd6ebd783ce162982627fe8544676dd4a62
MosheOgalbo <https://github.com/MosheOgalbo>
2021-10-21 21:20:45 +0300

    test v

--- index.py
+++ index.py
@@ -5,4 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_moshe():
     return "hello moshe"
+@app.route('/devops')
+def devops():
+    return "hello devops"
 app.run(host="0.0.0.0", port=5000)
